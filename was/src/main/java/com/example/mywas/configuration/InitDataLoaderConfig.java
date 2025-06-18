e7ac03999411869a226c7261c3dc7af84b5b97da
bwlim <=>
2025-06-18 15:37:37 +0900

    초기데이터 생성

--- was/src/main/java/com/example/mywas/configuration/InitDataLoaderConfig.java
+++ was/src/main/java/com/example/mywas/configuration/InitDataLoaderConfig.java
@@ -1,2 +1,12 @@
-package com.example.mywas.configuration;public class InitDataLoaderConfig {
+package com.example.mywas.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/*
+ 초기데이터를 위한 설정파일
+ 사용자, 식당, 메뉴 초기 데이터들을 springboot가 실행될때 초기화 되도록 한다
+*/
+@Configuration
+public class InitDataLoaderConfig {
+
 }
--- was/src/main/resources/sql/restraurant.sql
+++ was/src/main/resources/sql/restraurant.sql
@@ -10,12 +10,13 @@ CREATE TABLE restaurant (
 );
 
 CREATE TABLE menu (
-  id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  restaurant_id BIGINT,
-  name VARCHAR(255),
-  price INT,
-  description TEXT,
-  image_url TEXT,
-  score INT,
-  FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,           -- 기본 키, 자동 증가
+  restaurant_id BIGINT,                           -- 외래 키 대상 필드
+  name VARCHAR(255),                              -- 메뉴 이름
+  price INT,                                      -- 가격
+  description TEXT,                               -- 메뉴 설명 (긴 텍스트 가능)
+  image_url TEXT,                                 -- 이미지 URL
+  score INT,                                      -- 평점 또는 점수
+  FOREIGN KEY (restaurant_id) REFERENCES restaurant(id)  -- 외래 키 연결
 );
+
