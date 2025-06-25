0f6c624c9baf8db18725bea38ec7625bf9ef643a
bwlim <=>
2025-06-25 16:42:34 +0900

    웹소켓으로 주문알림가야하는데 어케 하는거냐 ..

--- was/src/main/java/com/example/mywas/configuration/WebSocketConfig.java
+++ was/src/main/java/com/example/mywas/configuration/WebSocketConfig.java
@@ -1,4 +1,23 @@
 package com.example.mywas.configuration;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic"); // 구독 채널
+        config.setApplicationDestinationPrefixes("/app"); // 전송 prefix
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-order") // WebSocket 연결 경로
+                .setAllowedOrigins("*")
+                .withSockJS(); // SockJS fallback 지원
+    }
 }
